1acc0cced6b0d58552c3a3990b0fa62d30759c1a
Kingsley Ijomah <git-example@codehance>
2024-02-12 13:26:45 +0700

    Remove  saved job

---
+++ jobplus-backend/src/api/saved-job/controllers/saved-job.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports = ({ strapi }) => ({
+  async saveJob(ctx) {
+    try {
+      ctx.body = await strapi
+        .service("api::saved-job.saved-job")
+        .saveJob(ctx.request.body);
+    } catch (error) {
+      strapi.log.error(error);
+    }
+  },
+  async removeSavedJob(ctx) {
+    try {
+      const { jobId, userId } = ctx.params;
+      ctx.body = await strapi
+        .service("api::saved-job.saved-job")
+        .removeSavedJob(jobId, userId);
+    } catch (error) {
+      strapi.log.error(error);
+    }
+  },
+});
---
+++ jobplus-backend/src/api/saved-job/services/saved-job.js
@@ -0,0 +1,58 @@
+"use strict";
+
+/**
+ * job service
+ */
+
+module.exports = ({ strapi }) => ({
+  async saveJob(data) {
+    try {
+      const entity = await strapi.entityService.create(
+        "api::saved-job.saved-job",
+        {
+          data: data,
+          populate: {
+            user: true,
+            job: true,
+          },
+        }
+      );
+      return entity;
+    } catch (error) {
+      strapi.log.error(`Error: ${error.message}`, {
+        data,
+      });
+      throw error;
+    }
+  },
+  async removeSavedJob(jobId, userId) {
+    try {
+      const savedJobs = await strapi.entityService.findMany("api::saved-job.saved-job", {
+        filters: {
+          job: jobId,
+          user: userId,
+        },
+        populate: {
+          user: {
+            fields: ['id', 'username', 'email']
+          },
+          job: {
+            fields: ['id', 'title']
+          },
+        },
+      });
+
+      // delete saved job
+      savedJobs.map(savedJob =>
+        strapi.entityService.delete("api::saved-job.saved-job", savedJob.id)
+      );
+
+      // return deleted saved job
+      return savedJobs;
+
+    } catch (error) {
+      strapi.log.error(`Error: ${error.message}`);
+      throw error;
+    }
+  },
+});
