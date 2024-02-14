d7c6de1405f38829080e3ab5a112cf9cd574051a
Kingsley Ijomah <git-example@codehance>
2024-02-14 16:35:01 +0700

    Adding  job card

--- jobplus-frontend/src/components/listings/listings.jsx
+++ jobplus-frontend/src/components/listings/listings.jsx
@@ -4,6 +4,7 @@ import './listings.scss';
 import Paginate from '../paginate/paginate';
 
 import { StarSaved, StarUnSaved, Money, Location, Timer } from '../images';
+import JobCard from '../job_card/job_card';
 
 import jobService from '../../services/JobService';
 import { Link } from 'react-router-dom';
@@ -27,7 +28,7 @@ export default function listings() {
   const { saveJob, removeSavedJob } = savedJobService();
   const { incrementSavedJobCount, decrementSavedJobCount } = useSavedJobCount();
 
-  const { getLoggedInUserId } = useAuth();
+  // const { getLoggedInUserId } = useAuth();
 
   const handleSuccess = (res) => {
     const { entries, meta } = res.data;
@@ -133,48 +134,10 @@ export default function listings() {
 
       <section>
         {jobs.map((job) => (
-          <div key={job.id} className="listing__card">
-            <header className="listing__header">
-              <h1 className="listing__title">
-                {job.title}
-              </h1>
-              <img className="listing__saved" src={ job.isSaved ? StarSaved : StarUnSaved } alt="" onClick={() => showModal(job)} />
-              <p className="listing__company">
-                Posted by <span>{job.company.name}</span>
-              </p>
-            </header>
-
-            <ul className="listing__items">
-              <li>
-                <img src={Money} alt="" />
-                <b>Salary {job.salaryType}</b>
-              </li>
-              <li>
-                <img src={Location} alt="" />
-                <b>{job.location}</b>
-              </li>
-              <li>
-                <img src={Timer} alt="" />
-                {job.job_types.map((type, index, array) => (
-                  <Fragment key={type.id}>
-                    <span>{type.title}</span>
-                    {index !== array.length - 1 && <span>, </span>}
-                  </Fragment>
-                ))}
-              </li>
-            </ul>
-
-            <p className="listing__detail">
-              {truncate(job.description, job.id)} 
-              <a onClick={() => toggleTruncate(job.id)}>
-                <b>{job.isTruncated ? 'Read more' : 'Read Less'}</b>
-              </a>
-            </p>
-
-            <Link to={`/apply/${job.id}`} className="listing__cta">
-                <b>{job.hasApplied ? 'Withdraw application' : 'Apply Now'}</b>
-            </Link>
-          </div>
+          <JobCard
+            key={job.id}
+            job={job}
+          />
         ))}
         <Paginate meta={meta.paginate} onPageChange={handlePageChange} />
       </section>
