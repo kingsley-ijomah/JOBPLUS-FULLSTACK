f6dbb21e88a6489e4e3df21d324d953b73bcd6ff
Kingsley Ijomah <git-example@codehance>
2024-02-22 17:00:29 +0700

    Filtering profiles 2

--- jobplus-backend/src/api/job/content-types/job/lifecycles.js
+++ jobplus-backend/src/api/job/content-types/job/lifecycles.js
@@ -4,11 +4,25 @@ module.exports = {
     // check if published
     if (!result.publishedAt) return;
 
+    // fetch one record matching result.id
+    const job = await strapi.entityService.findOne("api::job.job", result.id, {
+      populate: {
+        job_types: true,
+        category: {
+          populate: {
+            sector: true
+          }
+        }
+      }
+    });
+
     // Find matching profiles
     const matching_profiles = await strapi.entityService.findMany("api::profile.profile", {
       filters: {
-        desired_job_title: { $containsi: result.title},
-        min_per_anum_salary: { $lte: result.salary},
+        desired_job_title: { $containsi: job.title},
+        min_per_anum_salary: { $lte: job.salary},
+        job_types: { id: { $in: job.job_types.map((jt) => jt.id)}},
+        sector: { id: job.category.sector.id }
       }
     });
 
